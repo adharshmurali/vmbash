3242e5b073a0d961e016c2eeb96f710affcdaae7
Adharsh Murali <33362260+adharshmurali@users.noreply.github.com>
2020-05-19 08:54:34 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt-get install docker-ce
-sudo apt-get install python3-pip
-sudo apt-get install maven
-sudo apt-get install gradle
+sudo snap install docker
+sudo apt install python3-pip
+sudo apt install maven
+sudo apt install gradle
